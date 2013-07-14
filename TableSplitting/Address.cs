dafb3665a7235b43890c157960277a7b28301913
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2013-07-15 08:07:02 +1000

    Table splitting not working with many to many relationship

--- TableSplitting/Address.cs
+++ TableSplitting/Address.cs
@@ -1,9 +1,12 @@
-﻿namespace TableSplitting
+﻿using System.Collections.Generic;
+
+namespace TableSplitting
 {
     public class Address
     {
         public int Id { get; set; }
         public string Street { get; set; }
         public User User { get; set; }
+        public ICollection<Itinerary> Itineraries { get; set; }
     }
 }
